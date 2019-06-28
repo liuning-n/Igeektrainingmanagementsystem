9c7d2ac70bf1ad6d4bb55f4d62a5aad2bc8e2bd6
jarvis <1097077260@qq.com>
2019-06-28 19:36:08 +0800

    Initial commit

---
+++ src/main/java/com/igeek/IgeektrainingmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.igeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgeektrainingmanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgeektrainingmanagementsystemApplication.class, args);
+    }
+
+}
